3edc755f9e931c17353cce1edde2c1fdbbea7e57
rohitchauhan533 <chauhanrohit533@gmail.com>
2024-02-18 18:28:25 +0530

    reduxtp

---
+++ src/components/AddTodo.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+import { useDispatch } from 'react-redux'
+import {addTodo} from "../features/todo/todoSlice"
+
+const AddTodo = () => {
+
+   const [input, setInput] = useState("")
+   const dispatch = useDispatch()
+
+   const addToHandler = (e) => {
+    e.preventDefault()
+    dispatch(addTodo(input))
+    setInput("")
+   }
+  return (
+    <form onSubmit={addToHandler}>
+        <input type="text"
+         value={input}
+         onChange={(e)=> setInput(e.target.value)} />
+         <button type="submit">Add Todo</button>
+    </form>
+  )
+}
+
+export default AddTodo
